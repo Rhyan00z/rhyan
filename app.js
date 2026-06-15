8a061f9f8e64b255ef53e92838e359b2dca3c714
francisco.rhyan <francisco.rhyan@aluno.edu.pi.gov.br>
2026-06-15 15:16:51 +0000

    oi

---
+++ app.js
@@ -0,0 +1,9 @@
+const calculadora=require('./calculadora')
+let resultadosoma=calculadora.somar(1,2)
+console.log(resultadosoma);
+let resultadomultiplicar=calculadora.multiplicar(4,2)
+console.log(resultadomultiplicar);
+let resultadodividir=calculadora.dividir(6,2);
+console.log(resultadodividir);
+let resultadosubtrair=calculadora.subtrair(6,2)
+console.log(resultadosubtrair);
